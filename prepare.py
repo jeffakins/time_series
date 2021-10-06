ca9f9d8db27aafa520ecd3d8f81063c5e07b787e
Jeff Akins <jeffakins21@gmail.com>
2021-10-05 22:51:42 -0700

    add explore file

--- prepare.py
+++ prepare.py
@@ -19,3 +19,20 @@ def prepare_store_data():
     store_df['sales_total'] = store_df.sale_amount * store_df.item_price    # Creation of a sales total column
 
     return store_df
+
+
+
+# Prepare function for the opsd_germany_daily.csv data
+def prepare_opsd():
+    '''Function to prepare the ops data'''
+    opsd = ac.get_opsd_data()                                   # Acquire the data
+    opsd.Date = pd.to_datetime(opsd.Date)                       # Convert date column to pd datetime
+    opsd = opsd.set_index('Date').sort_index()                  # Sort by date
+    opsd = opsd.drop(columns='Unnamed: 0')                      # Drop random column
+    opsd['month'] = opsd.index.month_name()                     # Create month column
+    opsd['year'] = opsd.index.year                              # Create date column
+    opsd = opsd.fillna(0)                                       # Fill NaN with zero
+    opsd['wind_plus_solar'] = opsd['Wind'] + opsd['Solar']      # Calculate wind+solar
+    opsd = opsd.drop(columns='Wind+Solar')                      # Drop the Wind+Solar column due to missing calculations
+
+    return opsd
